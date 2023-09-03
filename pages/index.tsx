df363b8ebd62e6b6f7b6d5d9c20bc4a5c88e26d2
nawashiro <hooochiba@gmail.com>
2023-09-04 00:03:46 +0900

    formを切り替えできるように

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { ScreenReader } from "@capacitor/screen-reader";
 import { v4 as uuidv4 } from "uuid";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function HomePage() {
   const [host, setHost] = useState(""); //バックエンドのドメイン
   const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
   const [sessionId, setSessionId] = useState("");
+  const [formActive, setFormActive] = useState(true);
 
   const speak = async (text: string) => {
     await ScreenReader.speak({ value: text });
@@ -14,6 +15,9 @@ export default function HomePage() {
   const handleConnect = () => {
     //チェンネルへの接続
     if (host) {
+      //フォームを切り替える
+      setFormActive(false);
+
       //セッションIDを設定
       const newSessionId = uuidv4();
       console.log(`sessionId: ${newSessionId}`);
@@ -21,6 +25,7 @@ export default function HomePage() {
       //ホストを設定
       const newWebSocket = new WebSocket(`wss://${host}/streaming`);
 
+      //接続リクエスト送信
       newWebSocket.onopen = () => {
         setSessionId(newSessionId);
         setWebSocket(newWebSocket);
@@ -67,6 +72,10 @@ export default function HomePage() {
 
   //チャンネルを切断
   const disconnectChannel = () => {
+    //フォーム切り替え
+    setFormActive(true);
+
+    //切断リクエスト送信
     if (webSocket) {
       const data = {
         type: "disconnect",
@@ -87,16 +96,26 @@ export default function HomePage() {
   return (
     <>
       <h1>ashiga-kayui</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="example.com"
-          value={host}
-          onChange={(e) => setHost(e.target.value)}
-        ></input>
-        <button type="submit">Connect</button>
-      </form>
-      <button onClick={disconnectChannel}>disconnect</button>
+
+      <p>
+        ashiga-kayuiはMisskeyの投稿を自動読み上げするアプリです。
+        <br />
+        ログイン不要で使用することができます。
+      </p>
+      {formActive ? (
+        <form onSubmit={handleSubmit}>
+          <p>好きなMisskeyサーバーのドメイン名を入力してください。</p>
+          <input
+            type="text"
+            placeholder="example.com"
+            value={host}
+            onChange={(e) => setHost(e.target.value)}
+          ></input>
+          <button type="submit">Connect</button>
+        </form>
+      ) : (
+        <button onClick={disconnectChannel}>disconnect</button>
+      )}
     </>
   );
 }
